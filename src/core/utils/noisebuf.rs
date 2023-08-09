c68da25d0ee575351aa236def1a3afa26052816f
SpoogieOogie <sami.kharma@yahoo.de>
2023-08-09 03:59:18 +0200

    docs for noisebuf

--- src/core/utils/noisebuf.rs
+++ src/core/utils/noisebuf.rs
@@ -2,9 +2,52 @@ use crate::core::generator::Generator;
 use itertools::Itertools;
 use std::ops::{Index, IndexMut};
 
+/// A struct for generating an n-dimensional array and efficiently filling it with noise values.
+///
+/// This struct represents a simple n-dimensional array which is stored as a flat vector. When
+/// creating a new [`NoiseBuffer`] using the [`new()`] method, only the length along each
+/// dimension and a noise generator, that is, an object implementing [`Generator`], must be
+/// provided. The n-dimensional array is then filled with noise values. The implementation
+/// ensures, that the noise is computed in a way such that the underlying flat vector is written
+/// sequentially for maximal cache performance.
+/// 
+/// As [`NoiseBuffer`] implements the [`Index`] and [`IndexMut`] traits, it is possible to index
+/// the n-dimensional array with an index array of the appropriate length.
+/// 
+/// # Creating a noise buffer
+/// 
+/// A noise buffer can be easily and efficiently created using the [`new()`] method. The resulting
+/// buffer will be filled with noise values according to the provided noise generator.
+/// 
+/// When filling the buffer, the specific value at a given buffer index is computed by sampling
+/// the generator with the index interpreted as coordinates in n-dimensional space. This means
+/// that the buffer samples the generator on points of a hypergrid:
+/// 
+/// ```
+/// # use libnoise::{Source, Generator, NoiseBuffer};
+/// // create a generator
+/// let generator = Source::simplex(42);
+/// 
+/// // create a new noise buffer filled with noise values for each point
+/// let buf = NoiseBuffer::new([30, 20, 25], generator);
+/// 
+/// assert_eq!(buf[[17, 9, 21]], generator([17.0, 9.0, 21.0]));
+/// ```
+/// 
+/// The scale or position of the
+/// grid can be modified by calling adapters such as [`scale()`], [`translate()`], or [`rotate()`]
+/// on the generator before using it to create a [`NoiseBuffer`].
+/// 
+/// [`new()`]: NoiseBuffer::new
+/// [`scale()`]: Generator::scale
+/// [`translate()`]: Generator::translate
+/// [`rotate()`]: crate::Generator2D::rotate
 pub struct NoiseBuffer<const D: usize> {
+    /// Stores the length of the n-dimensional array along each dimension.
     pub shape: [usize; D],
-    pub(crate) offsets: [usize; D],
+    /// Stores offsets which are used to convert n-dimensional coordinates to flat vector indices.
+    pub offsets: [usize; D],
+    /// The underlying flat vector storing the noise values.
     pub buffer: Vec<f64>,
 }
 
@@ -30,6 +73,9 @@ macro_rules! impl_indexing {
 macro_rules! impl_new {
     ($dim:literal) => {
         impl NoiseBuffer<$dim> {
+            /// Creates a new noise buffer with the given `shape` and filled with noise generated
+            /// by the given `generator`. For further detail see the
+            /// [Creating a noise buffer](#creating-a-noise-buffer) section.
             pub fn new<G: Generator<$dim>>(shape: [usize; $dim], generator: G) -> Self {
                 let mut noisebuf = Self::new_empty(shape);
                 for point in noisebuf.tensor_indices() {
